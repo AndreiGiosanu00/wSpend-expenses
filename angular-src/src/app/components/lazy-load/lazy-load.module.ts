d06ee1310a4508348e3f5968cc3d4e5c32d05928
Andrei-se <andrei.g@spamexperts.com>
2019-06-23 18:33:52 +0300

    Add guard for auth component.

--- angular-src/src/app/components/lazy-load/lazy-load.module.ts
+++ angular-src/src/app/components/lazy-load/lazy-load.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes,RouterModule } from '@angular/router';
+import {AuthGuard} from "../../guards/auth.guard";
 
 const routes: Routes = [
-    {path: 'auth', loadChildren: '../auth/auth.module#AuthModule'},
+    {path: 'auth', loadChildren: '../auth/auth.module#AuthModule', canActivate: [AuthGuard]},
     {path: 'register', loadChildren: '../register/register.module#RegisterModule'},
     {path: 'login', loadChildren: '../pages/login/login.module#LoginModule'},
 
