720ce6bab52ea6316859cbd4518d0646a0119978
Andrei-se <andrei.g@spamexperts.com>
2019-06-23 18:40:06 +0300

    Fix dashboard menu links.

--- angular-src/src/app/components/expenses/expenses.component.ts
+++ angular-src/src/app/components/expenses/expenses.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
 
 declare let $: any;
 
@@ -9,32 +11,9 @@ declare let $: any;
 })
 export class ExpensesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService,
+              private router: Router) {}
 
-  ngOnInit() {
-    $(document).ready(function(){
-      // Activate tooltip
-      $('[data-toggle="tooltip"]').tooltip();
-
-      // Select/Deselect checkboxes
-      var checkbox = $('table tbody input[type="checkbox"]');
-      $("#selectAll").click(function(){
-        if(this.checked){
-          checkbox.each(function(){
-            this.checked = true;
-          });
-        } else{
-          checkbox.each(function(){
-            this.checked = false;
-          });
-        }
-      });
-      checkbox.click(function(){
-        if(!this.checked){
-          $("#selectAll").prop("checked", false);
-        }
-      });
-    });
-  }
+  ngOnInit() {}
 
 }
