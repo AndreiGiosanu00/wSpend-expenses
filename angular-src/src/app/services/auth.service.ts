43b6aafefe70af3bd6214c437e774f09759156f5
Andrei-se <andrei.g@spamexperts.com>
2019-06-24 16:23:16 +0300

    Update the auth service with API support for goals and expenses.

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -8,6 +8,9 @@ export class AuthService {
 
   authToken: any;
   user: any;
+  expense: any;
+  goal: any;
+
 
   constructor (public http: HttpClient) {}
 
@@ -33,6 +36,89 @@ export class AuthService {
 
   }
 
+  getProfile() {
+
+    let headers =  new HttpHeaders();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type', 'application/json');
+
+    return this.http.get('http://localhost:3000/users/profile',{headers: headers});
+
+  }
+
+  addExpense(expenseWithUsername) {
+    let headers =  new HttpHeaders();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type', 'application/json');
+
+    return this.http.post('http://localhost:3000/expenses/add_expense', expenseWithUsername, {headers: headers});
+  }
+
+  getAllExpenses() {
+    let headers =  new HttpHeaders();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type', 'application/json');
+
+    return this.http.get('http://localhost:3000/expenses/', {headers: headers});
+  }
+
+  updateExpense(expenseUpdated) {
+    let headers =  new HttpHeaders();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type', 'application/json');
+
+    return this.http.put('http://localhost:3000/expenses/update_expense/' + expenseUpdated._id, expenseUpdated,{headers: headers});
+  }
+
+  deleteExpense(expenseId) {
+    let headers =  new HttpHeaders();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type', 'application/json');
+
+    return this.http.delete('http://localhost:3000/expenses/delete_expense/' + expenseId,{headers: headers});
+  }
+
+  addGoal(goal) {
+    let headers =  new HttpHeaders();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type', 'application/json');
+
+    return this.http.post('http://localhost:3000/goals/add_goal', goal, {headers: headers});
+  }
+
+  getAllGoals() {
+    let headers =  new HttpHeaders();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type', 'application/json');
+
+    return this.http.get('http://localhost:3000/goals/', {headers: headers});
+  }
+
+  updateGoal(goalUpdated) {
+    let headers =  new HttpHeaders();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type', 'application/json');
+
+    return this.http.put('http://localhost:3000/goals/update_goal/' + goalUpdated._id, goalUpdated,{headers: headers});
+  }
+
+  deleteGoal(goalId) {
+    let headers =  new HttpHeaders();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type', 'application/json');
+
+    return this.http.delete('http://localhost:3000/goals/delete_goal/' + goalId,{headers: headers});
+  }
+
   storeUserData(token, user) {
 
     localStorage.setItem('id_token', token);
@@ -47,17 +133,6 @@ export class AuthService {
 
     this.authToken = this.user = null;
     localStorage.clear();
-
-  }
-
-  getProfile() {
-
-    let headers =  new HttpHeaders();
-    this.loadToken();
-    headers.append('Authorization', this.authToken);
-    headers.append('Content-Type', 'application/json');
-
-    return this.http.get('http://localhost:3000/users/profile',{headers: headers});
 
   }
 
@@ -77,9 +152,5 @@ export class AuthService {
     }
 
     return  false;
-
   }
-
-
-
 }
